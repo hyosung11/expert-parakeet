9a52b70a0e16245a0bebda2bf70e13e9e5036ab1
HyoSung Bidol-Lee <hyosung11@me.com>
2023-09-18 09:48:03 -0400

    Create static assets directories

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// const express = require("express");
+const express = require("express");
+const app = express();
 
-console.log("Hello, world");
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("layout");
+});
+
+app.listen(3000, "localhost", () => {
+  console.log("Listening to port 3000.");
+});
